427916fa1aaf73daa2653ee16ed1ae3e237dc9cb
Justus marwa <justusmwita@gmail.com>
2022-04-21 10:43:41 +0000

    added newsletter functionalityv

--- profiles/models.py
+++ profiles/models.py
@@ -33,3 +33,7 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
     instance.userprofile.save()
+
+class SubscribedUsers(models.Model):
+    email = models.CharField(unique=True, max_length=50)
+    name = models.CharField(max_length=50)
--- profiles/views.py
+++ profiles/views.py
@@ -3,7 +3,11 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
-
+from django.http import JsonResponse
+import re
+from .models import SubscribedUsers
+from django.core.mail import send_mail
+from django.conf import settings
 from checkout.models import Order
 
 
@@ -46,5 +50,35 @@ def order_history(request, order_number):
         'order': order,
         'from_profile': True,
     }
+    def index(request):
+    if request.method == 'POST':
+        post_data = request.POST.copy()
+        email = post_data.get("email", None)
+        name = post_data.get("name", None)
+        subscribedUsers = SubscribedUsers()
+        subscribedUsers.email = email
+        subscribedUsers.name = name
+        subscribedUsers.save()
+        # send a confirmation mail
+        subject = 'NewsLetter Subscription'
+        message = 'Hello ' + name + ', Thanks for subscribing us. You will get notification of latest articles posted on our website. Please do not reply on this email.'
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [email, ]
+        send_mail(subject, message, email_from, recipient_list)
+        res = JsonResponse({'msg': 'Thanks. Subscribed Successfully!'})
+        return res
+    return render(request, 'index.html')
+
+def validate_email(request): 
+    email = request.POST.get("email", None)   
+    if email is None:
+        res = JsonResponse({'msg': 'Email is required.'})
+    elif SubscribedUsers.objects.get(email = email):
+        res = JsonResponse({'msg': 'Email Address already exists'})
+    elif not re.match(r"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", email):
+        res = JsonResponse({'msg': 'Invalid Email Address'})
+    else:
+        res = JsonResponse({'msg': ''})
+    return res
 
     return render(request, template, context)
